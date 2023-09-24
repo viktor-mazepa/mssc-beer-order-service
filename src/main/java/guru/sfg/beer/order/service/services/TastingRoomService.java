86edb1b7804a0ee603e922a439b812a73039474a
viktor-mazepa <viktor.mazepa@gmail.com>
2023-09-24 17:04:58 +0300

    Beer Order Service collaboration Implementation

--- src/main/java/guru/sfg/beer/order/service/services/TastingRoomService.java
+++ src/main/java/guru/sfg/beer/order/service/services/TastingRoomService.java
@@ -38,11 +38,11 @@ public class TastingRoomService {
 
     @Transactional
     @Scheduled(fixedRate = 2000) //run every 2 seconds
-    public void placeTastingRoomOrder(){
+    public void placeTastingRoomOrder() {
 
         List<Customer> customerList = customerRepository.findAllByCustomerNameLike(BeerOrderBootStrap.TASTING_ROOM);
 
-        if (customerList.size() == 1){ //should be just one
+        if (customerList.size() == 1) { //should be just one
             doPlaceOrder(customerList.get(0));
         } else {
             log.error("Too many or too few tasting room customers found");
@@ -67,10 +67,9 @@ public class TastingRoomService {
                 .build();
 
         BeerOrderDto savedOrder = beerOrderService.placeOrder(customer.getId(), beerOrder);
-
     }
 
     private String getRandomBeerUpc() {
-        return beerUpcs.get(new Random().nextInt(beerUpcs.size() -0));
+        return beerUpcs.get(new Random().nextInt(beerUpcs.size() - 0));
     }
 }
